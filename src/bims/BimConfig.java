6f1a36f50cd532055b3cc9060178ddf984963c80
themadpillow <mouhanpatumakura@gmail.com>
2018-10-30 01:05:00 +0900

    defaultのBimConfigの作成

---
+++ src/bims/BimConfig.java
@@ -0,0 +1,76 @@
+package bims;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+import other.CustomConfig;
+
+public class BimConfig {
+	private CustomConfig configuration;
+
+	private static float defaultThrowRange;
+	private static float runThrowRange;
+	private static float shiftThrowRange;
+
+	private static float timerBimDamage;
+	private static float crackBimDamage;
+	private static float homingBimDamage;
+	private static float installationBimDamage;
+
+	private static float homingSpeed;
+	private static float installationRange;
+
+	public BimConfig(CustomConfig config) {
+		configuration = config;
+		init();
+	}
+
+	private void init() {
+		FileConfiguration config = configuration.getConfig();
+
+		defaultThrowRange = (float) config.getDouble("ThrowRange.default");
+		runThrowRange = (float) config.getDouble("ThrowRange.run");
+		shiftThrowRange = (float) config.getDouble("ThrowRange.shift");
+
+		timerBimDamage = (float) config.getDouble("Bims.TimerBim.damage");
+		crackBimDamage = (float) config.getDouble("Bims.CrackerBim.damage");
+		homingBimDamage = (float) config.getDouble("Bims.HomingBim.damage");
+		installationBimDamage = (float) config.getDouble("Bims.InstallationBim.damage");
+
+		homingSpeed = (float) config.getDouble("Bims.HomingBim.speed");
+		installationRange = (float) config.getDouble("Bims.InstallationBim.range");
+	}
+
+	public static float getDamage(Bims bim) {
+		if (bim == Bims.TimerBim) {
+			return timerBimDamage;
+		} else if (bim == Bims.CrackerBim) {
+			return crackBimDamage;
+		} else if (bim == Bims.HomingBim) {
+			return homingBimDamage;
+		} else if (bim == Bims.InstallationBim) {
+			return installationBimDamage;
+		} else {
+			throw new RuntimeException("不明なBimが指定されました");
+		}
+	}
+
+	public static float getHomingSpeed() {
+		return homingSpeed;
+	}
+
+	public static float getInstallationRange() {
+		return installationRange;
+	}
+
+	public static float getThrowRange(Status status) {
+		if (status == bims.Status.Default) {
+			return defaultThrowRange;
+		} else if (status == bims.Status.Run) {
+			return runThrowRange;
+		} else if (status == bims.Status.Shift) {
+			return shiftThrowRange;
+		} else {
+			throw new RuntimeException("不明なStatusが指定されました");
+		}
+	}
+}
---
+++ src/bims/Status.java
@@ -0,0 +1,5 @@
+package bims;
+
+public enum Status {
+	Default, Run, Shift;
+}
